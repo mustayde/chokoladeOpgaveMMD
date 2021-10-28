3f3218e5af393de786ce8c6d823bc900a7dfd973
mustayde <35762106+mustayde@users.noreply.github.com>
2021-10-28 13:33:17 +0200

    Add files via upload

---
+++ Website/javascript.js
@@ -0,0 +1,10 @@
+/* åbner mobil menuen når element med onclick="openNav() klikkes*/
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* lukker mobil menuen når element med onclick="closeNav() klikkes*/
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+
+}
